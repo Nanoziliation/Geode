03934bde275321961eb7c1bcb23213acdef9d7c9
Andreos <egajda@umich.edu>
2018-05-28 00:51:35 -0400

    Update Article model

--- src/server/models/article.js
+++ src/server/models/article.js
@@ -1,18 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
+const ArticleModel = (sequelize, DataTypes) => {
   const Article = sequelize.define('Article', {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
-    content: DataTypes.STRING,
+
+    content: DataTypes.TEXT,
+
+    order: DataTypes.INTEGER,
+
     editable: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
     },
-    order: DataTypes.INTEGER
-  }, {})
-  Article.associate = (models) => {
-    // associations can be defined here
-  }
-  return Article;
+
+  })
+
+  return Article
 }
+
+export default ArticleModel
